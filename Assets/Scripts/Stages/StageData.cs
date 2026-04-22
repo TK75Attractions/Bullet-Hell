53248526e31c1869237adca9452fb1ff21e87686
Raymeela <raymee675@gmail.com>
2026-04-22 14:44:58 +0900

    push

--- Assets/Scripts/Stages/StageData.cs
+++ Assets/Scripts/Stages/StageData.cs
@@ -11,6 +11,32 @@ public class StageData : ScriptableObject
     public int difficulty;//0:easy 1:normal 2:hard 3:lunatic
     public string stageName;
     public VideoClip videoClip;
+    public AudioClip audioClip;
+    public List<MusicEvent> MusicEvents;
+
+    [System.Serializable]
+    public class MusicEvent
+    {
+        public int barCount;
+        public float BPM;
+        public List<int> beatTimings;
+        public int measure;
+
+        public void Refresh()
+        {
+            List<int> newBeatTimings = new List<int>();
+            for (int i = 0; i < beatTimings.Count; i++)
+            {
+                if (beatTimings[i] < measure)
+                {
+                    if (newBeatTimings.Contains(beatTimings[i])) continue;
+                    newBeatTimings.Add(beatTimings[i]);
+                }
+                else continue;
+            }
+            beatTimings = newBeatTimings;
+        }
+    }
 
     [TextArea]
     public string stageDescription;
